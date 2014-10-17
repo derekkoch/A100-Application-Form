edcc8e64e300264ea4c61b12df6e20bcb8f57c36
Paul J Burinda <pburi1@unh.newhaven.edu>
2014-10-17 18:51:35 -0400

    Delete cred_ext.php

--- resources/cred_ext.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if (!defined('USER')) define('USER', 'root');
-    if (!defined('PASSWORD')) define('PASSWORD', 'schecter4l');
-    if (!defined('HOST')) define('HOST', 'localhost');
-    if (!defined('DATABASE_NAME')) define('DATABASE_NAME', 'applications_db');
-?>
