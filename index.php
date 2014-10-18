e0aff2bea37575a5cdecab9693dbdfc76ef9ba7e
yan <yanren2013@gmail.com>
2014-10-18 12:30:39 -0400

    fix index.php

--- index.php
+++ index.php
@@ -27,13 +27,9 @@
 	<!-- PHP to pull Cohort Dropdown Options -->
 	<?php
 	include "admin/db_conn.php";
-	$dbh = dbconn();
-
-
-		<!-- PHP to pull Cohort Dropdown Options -->
-		<?php
-			include "resources/cred_int.php";
-		?>
+	$dbh = dbconn();	
+	include "resources/cred_int.php";
+	?>
 
 
 
